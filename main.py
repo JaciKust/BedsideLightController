c15eeca81aa05c9c4e098943dd8ee70e09615319
Jaci Kust <jaci.kust@protonmail.com>
2020-09-13 12:33:05 -0600

    refactored main.py's button_callback method by extracting out methods making it much easier to read.

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from Constants import Color as ColorConstant
 from Constants import PrimaryButton as PrimaryButtonConstant
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)  # Ignore warning for now
-
+GPIO.setwarnings(False)
 GPIO.setup(PrimaryButtonConstant.BLUE_PIN, GPIO.OUT)
 GPIO.setup(PrimaryButtonConstant.RED_PIN, GPIO.OUT)
 GPIO.setup(PrimaryButtonConstant.GREEN_PIN, GPIO.OUT)
@@ -16,86 +15,88 @@ LED_MAXIMUM = 100
 blue_led = GPIO.PWM(PrimaryButtonConstant.BLUE_PIN, LED_MAXIMUM)
 red_led = GPIO.PWM(PrimaryButtonConstant.RED_PIN, LED_MAXIMUM)
 green_led = GPIO.PWM(PrimaryButtonConstant.GREEN_PIN, LED_MAXIMUM)
+
 previous_color = None
+current_state = AwakeLightsOnState()
+current_state.execute_state_change()
 
 
-def set_led_color(color):
+def set_button_color(color):
     global previous_color
     if previous_color == color:
         return
     previous_color = color
-    print("Setting color to: " + str(color))
     red_led.ChangeDutyCycle(LED_MAXIMUM - color[ColorConstant.RED_LOCATION])
     green_led.ChangeDutyCycle(LED_MAXIMUM - color[ColorConstant.GREEN_LOCATION])
     blue_led.ChangeDutyCycle(LED_MAXIMUM - color[ColorConstant.BLUE_LOCATION])
 
 
-current_state = AwakeLightsOnState()
-current_state.execute_state_change()
-
-
-def button_callback(channel):
+def on_button_press(channel):
     global current_state
     start_time = time.time()
     button_time = 0
+    has_long_press_been_set = False
+    has_short_press_been_set = False
     time.sleep(0.01)
-    print('button pressed')
-    while GPIO.input(channel) == PrimaryButtonConstant.BUTTON_PRESSED_VALUE and button_time < PrimaryButtonConstant.EXTRA_LONG_PRESS_MIN:  # Wait for the button up
-        print("In button loop")
-        button_time = time.time() - start_time
-        set_long_press = False
-        set_press = False
-        if not set_long_press and button_time >= PrimaryButtonConstant.LONG_PRESS_MIN:
-            set_long_press = True
-            set_led_color(current_state.get_ring_color_on_long_press())
-        elif not set_press:
-            set_press = True
-            set_led_color(current_state.get_ring_color_on_press())
-        time.sleep(0.1)
 
-    print('continuing')
+    while GPIO.input(channel) == PrimaryButtonConstant.BUTTON_PRESSED_VALUE and \
+            button_time < PrimaryButtonConstant.EXTRA_LONG_PRESS_MIN:  # Wait for the button up
+        button_time, has_long_press_been_set, has_short_press_been_set = \
+            handle_button_color(start_time, has_long_press_been_set, has_short_press_been_set)
 
-    is_noise_press = False
+    set_new_state(get_new_state(button_time))
+    wait_for_button_release(channel)
 
-    new_state = None
-    # extra long press
-    if button_time >= PrimaryButtonConstant.EXTRA_LONG_PRESS_MIN:
-        new_state = current_state.on_extra_long_press()
-    # long button press
-    elif button_time >= PrimaryButtonConstant.LONG_PRESS_MIN:
-        new_state = current_state.on_long_press()
-    # short press
-    elif button_time >= PrimaryButtonConstant.NOISE_THRESHOLD:
-        new_state = current_state.on_short_press()
-    # noise
+
+def set_new_state(state):
+    global current_state
+    if state is None:
+        set_button_color(current_state.get_ring_color())
     else:
-        print("Was determined to be noise")
-        is_noise_press = True
-
-    if new_state is None:
-        print("-----------> No state change detected.")
-        set_led_color(current_state.get_ring_color())
-    else:
-        current_state = new_state
-        set_led_color(current_state.get_ring_color())
+        current_state = state
+        set_button_color(current_state.get_ring_color())
         current_state.execute_state_change()
 
-    if not is_noise_press:
-        # Wait for button to be released if still pressed for extra long press.
-        while GPIO.input(channel) == PrimaryButtonConstant.BUTTON_PRESSED_VALUE:
-            time.sleep(0.1)
 
-        print("Press time: " + str(button_time))
+def wait_for_button_release(channel):
+    while GPIO.input(channel) == PrimaryButtonConstant.BUTTON_PRESSED_VALUE:
+        time.sleep(0.1)
+
+
+def get_new_state(button_time):
+    return_state = None
+    # extra long press
+    if button_time >= PrimaryButtonConstant.EXTRA_LONG_PRESS_MIN:
+        return_state = current_state.on_extra_long_press()
+    # long button press
+    elif button_time >= PrimaryButtonConstant.LONG_PRESS_MIN:
+        return_state = current_state.on_long_press()
+    # short press
+    elif button_time >= PrimaryButtonConstant.NOISE_THRESHOLD:
+        return_state = current_state.on_short_press()
+    return return_state
+
+
+def handle_button_color(start_time, has_long_press_been_set, has_short_press_been_set):
+    button_time = time.time() - start_time
+    if not has_long_press_been_set and button_time >= PrimaryButtonConstant.LONG_PRESS_MIN:
+        has_long_press_been_set = True
+        set_button_color(current_state.get_ring_color_on_long_press())
+    elif not has_short_press_been_set:
+        has_short_press_been_set = True
+        set_button_color(current_state.get_ring_color_on_press())
+    time.sleep(0.1)
+    return button_time, has_long_press_been_set, has_short_press_been_set
 
 
 if __name__ == '__main__':
     red_led.start(100)
     green_led.start(100)
     blue_led.start(100)
-    set_led_color(current_state.get_ring_color())
+    set_button_color(current_state.get_ring_color())
 
     GPIO.setup(PrimaryButtonConstant.TRIGGER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(PrimaryButtonConstant.TRIGGER_PIN, GPIO.RISING, callback=button_callback, bouncetime=PrimaryButtonConstant.BOUNCE_TIME_MS)
+    GPIO.add_event_detect(PrimaryButtonConstant.TRIGGER_PIN, GPIO.RISING, callback=on_button_press, bouncetime=PrimaryButtonConstant.BOUNCE_TIME_MS)
     print("Current time: " + str(datetime.now()))
 
     while True:
@@ -107,4 +108,4 @@ if __name__ == '__main__':
             print("Executing state change based on time")
             current_state = new_state
             current_state.execute_state_change()
-            set_led_color(current_state.get_ring_color())
+            set_button_color(current_state.get_ring_color())
