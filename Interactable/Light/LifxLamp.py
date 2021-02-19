daabacc225b3498039d6e47313d6675636c801e6
Jaci Kust <jaci.kust@protonmail.com>
2021-02-18 21:53:46 -0700

    Lifx lights are now actually turned off instead of being set to 'black'. Color is now stored in the Light and is accessible.

--- Interactable/Light/LifxLamp.py
+++ Interactable/Light/LifxLamp.py
@@ -2,8 +2,6 @@ import threading
 
 from lifxlan import Group
 
-import Color as ColorConstant
-
 
 # import Interactable.Light.Lamp as Lamp
 
@@ -13,6 +11,8 @@ class LifxLamp():
         self.wrapped_lights = self._get_wrapped_lights(lifx_lights)
         self.lifx_lifx_lights = lifx_lights
         self.group = Group(self.wrapped_lights)
+        self.current_color = None
+        self.is_off = True
 
     default_transition_time = 200
 
@@ -20,19 +20,16 @@ class LifxLamp():
         return list(map(lambda l: l.wrapped_bulb, lifx_lights))
 
     def turn_on(self, color, transition_time=None):
-        try:
-            self._set_group(color, transition_time)
-        finally:
-            self.is_on = True
+        self._set_group(color, transition_time)
 
     def turn_off(self, transition_time=None):
-        try:
-            self._set_group(ColorConstant.BLACK, transition_time)
-        finally:
-            self.is_on = False
+        self._set_group(None, transition_time)
 
     def get_is_on(self):
-        return self.is_on
+        return not self.is_off
+
+    def get_current_color(self):
+        return self.current_color
 
     def can_handle_kelvin(self):
         return True
@@ -41,14 +38,26 @@ class LifxLamp():
         return self.default_transition_time if time is None else time
 
     def _set_group(self, color, transition_time):
-        threading.Thread(target=self._run, args=(color, transition_time)).start()
+        turn_off = color is None
+        if not turn_off:
+            self.current_color = color
+        threading.Thread(target=self._run, args=(color, transition_time, turn_off)).start()
 
-    def _run(self, color, transition_time):
+    def _run(self, color, transition_time, turn_off):
         transition_time = self._get_transition_time(transition_time)
         num_tries = 5
         for x in range(num_tries):
             try:
-                self.group.set_color(color.as_hsv_array(), transition_time)
+                if turn_off:
+                    self.group.set_power(False, transition_time)
+                    self.is_off = True
+                else:
+                    if self.is_off:
+                        self.group.set_color(color.as_hsv_array(), 0)
+                        self.group.set_power(True, transition_time)
+                        self.is_off = False
+                    else:
+                        self.group.set_color(color.as_hsv_array(), transition_time)
             except:
                 pass
             else:
