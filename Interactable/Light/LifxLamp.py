21e5c81727e6b7e8b127263e2a216e01397c445d
Jaci Kust <jaci.kust@protonmail.com>
2021-02-23 17:09:17 -0700

    Fixed bug where lights wouldn't sink up if a group's status was changed.

--- Interactable/Light/LifxLamp.py
+++ Interactable/Light/LifxLamp.py
@@ -6,28 +6,20 @@ from lifxlan import Group
 class LifxLamp():
     def __init__(self, lifx_lights, name):
         self.name = name
-        self.wrapped_lights = self._get_wrapped_lights(lifx_lights)
+        # self.wrapped_lights = self._get_wrapped_lights(lifx_lights)
         self.lifx_lifx_lights = lifx_lights
-        self.group = Group(self.wrapped_lights)
-        self.current_color = None
-        self.is_off = True
+        # self.group = Group(self.wrapped_lights)
 
     default_transition_time = 200
 
-    def _get_wrapped_lights(self, lifx_lights):
-        return list(map(lambda l: l.wrapped_bulb, lifx_lights))
+    def _group(self, lifx_lights):
+        return Group(list(map(lambda l: l.wrapped_bulb, lifx_lights)))
 
     def turn_on(self, color, transition_time=None):
         self._set_group(color, transition_time)
 
     def turn_off(self, transition_time=None):
         self._set_group(None, transition_time)
-
-    def get_is_on(self):
-        return not self.is_off
-
-    def get_current_color(self):
-        return self.current_color
 
     def can_handle_kelvin(self):
         return True
@@ -36,27 +28,59 @@ class LifxLamp():
         return self.default_transition_time if time is None else time
 
     def _set_group(self, color, transition_time):
+        on_group = self._group(list(filter(lambda x: x.get_is_on(), self.lifx_lifx_lights)))
+        off_group = self._group(list(filter(lambda x: not x.get_is_on(), self.lifx_lifx_lights)))
         turn_off = color is None
-        if not turn_off:
-            self.current_color = color
-        threading.Thread(target=self._run, args=(color, transition_time, turn_off)).start()
 
-    def _run(self, color, transition_time, turn_off):
+        if turn_off:
+            threading.Thread(target=self._turn_off, args=(on_group, transition_time)).start()
+        else:
+            threading.Thread(target=self._turn_on, args=(on_group, off_group, color, transition_time)).start()
+
+    def _turn_on(self, on_group, off_group, color, transition_time):
         transition_time = self._get_transition_time(transition_time)
         num_tries = 5
+
         for x in range(num_tries):
             try:
-                if turn_off:
-                    self.group.set_power(False, transition_time)
-                    self.is_off = True
-                else:
-                    if self.is_off:
-                        self.group.set_color(color.as_hsv_array(), 0)
-                        self.group.set_power(True, transition_time)
-                        self.is_off = False
-                    else:
-                        self.group.set_color(color.as_hsv_array(), transition_time)
+                off_group.set_color(color.as_hsv_array(), 0)
             except:
                 pass
             else:
                 break
+
+        for x in range(num_tries):
+            try:
+                off_group.set_power(True, transition_time)
+            except:
+                pass
+            else:
+                break
+
+        for x in range(num_tries):
+            try:
+                on_group.set_color(color.as_hsv_array(), transition_time)
+            except:
+                pass
+            else:
+                break
+
+        for light in self.lifx_lifx_lights:
+            light.is_off = False
+            light.current_color = color
+
+    def _turn_off(self, group, transition_time):
+        transition_time = self._get_transition_time(transition_time)
+        num_tries = 5
+        for x in range(num_tries):
+            try:
+                group.set_power(False, transition_time)
+                self.is_off = True
+            except:
+                pass
+            else:
+                break
+
+        for light in self.lifx_lifx_lights:
+            light.is_off = True
+            light.current_color = None
